8919dc24c800bd1a3d1f78231276a662e73ff320
smsaxena19 <smsaxena19@gmail.com>
2024-07-10 21:22:23 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webappforgithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
